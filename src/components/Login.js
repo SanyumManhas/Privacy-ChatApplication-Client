aeed378988004c044a2043b6cd037fbfb6c0edd8
SanyumManhas <90847147+SanyumManhas@users.noreply.github.com>
2025-08-13 12:55:27 +0530

    Update Login.js, Added JWT to localStorage for IOS

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,6 +26,7 @@ export default function Login()
       if(result.data.success)
       {
         setuserdata(result.data.userdetails);
+        localStorage.setItem("token", result.data.token);
         nav("/home");
       }
       else
